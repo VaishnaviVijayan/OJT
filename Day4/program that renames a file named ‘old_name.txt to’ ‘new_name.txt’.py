ee929a69605801691344f666745012758a9e23b4
VaishnaviVijayan <122439807+VaishnaviVijayan@users.noreply.github.com>
2024-05-22 11:22:26 +0530

    Delete Day4 directory

--- Day4/program that renames a file named ‘old_name.txt to’ ‘new_name.txt’.py
+++
@@ -1,6 +0,0 @@
-import os
-
-old_name = "details.txt"
-new_name = "new_details.txt"
-
-os.rename(old_name, new_name)
--- Day4/program to Read or Write to a File.py
+++
@@ -1,7 +0,0 @@
-f = open("demofile2.txt", "a")
-f.write("hehehehehehehe!")
-f.close()
-
-#open and read the file after the appending:
-f = open("demofile2.txt", "r")
-print(f.read())
