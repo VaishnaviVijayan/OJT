c9d91563d3cabc808cacbca38696d4d11e0cd430
VaishnaviVijayan <122439807+VaishnaviVijayan@users.noreply.github.com>
2024-05-31 09:44:16 +0530

    Add files via upload

---
+++ Day10/NumPy nd array Object.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Create a 1D array
+array_1d = np.array([1, 2, 3, 4, 5])
+print("1D array:")
+print(array_1d)
+
+# Create a 2D array
+array_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print("\n2D array:")
+print(array_2d)
+
+# Create a 3D array
+array_3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print("\n3D array:")
+print(array_3d)
